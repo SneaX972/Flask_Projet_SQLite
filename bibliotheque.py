251c8e18baf5b5fd3eea126a4d4b627304baae0b
SneaX972 <cantanicolas@gmail.com>
2025-03-25 11:43:41 +0100

    Update bibliotheque.py

--- bibliotheque.py
+++ bibliotheque.py
@@ -6,14 +6,14 @@ connection = sqlite3.connect('bibliotheque.db')
 # Lecture et exécution du fichier SQL dédié aux livres et emprunts
 with open('schema.sql') as f:
     connection.executescript(f.read())
-
+cur = connection.cursor()
 # Ajouter un utilisateur adminn
 cur.execute("""
 INSERT INTO Utilisateurs (Canta, Nico, Email, Mot_de_passe, Role)
 VALUES (?, ?, ?, ?, ?)
 """, ('Admin', 'Super', 'test@biblio.com', 'password', 'admin'))
 
-cur = connection.cursor()
+
 
 # Remplissage initial des livres
 cur.execute("INSERT INTO livres (titre, auteur, genre, disponible) VALUES (?, ?, ?, ?)",
